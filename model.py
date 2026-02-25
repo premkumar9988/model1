00517c341394558b73903777635d95b2de5acd49
premkumar9988 <premkumar00786337@gmail.com>
2026-02-25 12:46:57 +0530

    changed

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 def greet(name, age, country):
     print(f"Hii, i am {name}! my age is {age} and i am from {country}.")
-
 greet("Prem", 30, "USA")
 greet("Alice", 25, "UK")
 greet("Bob", 35, "Canada")
+greet()
 
 
 a=10
