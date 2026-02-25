224037a83bda428f9cd423850f80ce686c04c11a
premkumar9988 <premkumar00786337@gmail.com>
2026-02-25 12:08:52 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,6 @@
+def greet(name, age, country):
+    print(f"Hii, i am {name}! my age is {age} and i am from {country}.")
+
+greet("Prem", 30, "USA")
+greet("Alice", 25, "UK")
+greet("Bob", 35, "Canada")
