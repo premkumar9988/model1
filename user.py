8aab92ff0d24d9fc77d7f76658ee6c927816eaf5
premkumar9988 <premkumar00786337@gmail.com>
2026-02-25 12:21:40 +0530

    first commit

---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
